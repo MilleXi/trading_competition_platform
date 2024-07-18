cbbc3df3bc7afc483ae58f320da9db24c4a203f9
MilleX <xyy318@126.com>
2024-07-18 19:04:03 +0800

    change the name of the game

--- frontend/src/components/home/GameIntro.js
+++ frontend/src/components/home/GameIntro.js
@@ -3,7 +3,7 @@ import React from 'react';
 const GameIntro = () => {
   return (
     <div className="game-intro text-center">
-      <h1 className="game-title">Investment Competition</h1>
+      <h1 className="game-title">Trading Wars</h1>
       <p className="game-description">
       Welcome to the Investment Competition! Here, you'll compete against AI,
        using your strategies and skills to see if you can outperform the AI and become the stock market champion!
